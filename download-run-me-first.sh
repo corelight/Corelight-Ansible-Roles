8fadcf32dc2af3d0858ccc9345922b87509ce3a1
James Lagermann <james.lagermann@corelight.com>
2020-12-07 17:34:44 -0600

    updated run-me-first.sh
    Signed-off-by: James Lagermann <james.lagermann@corelight.com>

--- download-run-me-first.sh
+++ download-run-me-first.sh
@@ -1,24 +1,31 @@
 #!
 
-sudo mkdir /etc/corelight
-cd /etc/corelight
-sudo chown $USER.$USER /etc/corelight
-cd /etc/corelight
+sudo mkdir /etc/corelight-env
+cd /etc/corelight-env
+sudo chown $USER.$USER /etc/corelight-env
+cd /etc/corelight-env
 git clone https://github.com/corelight/Corelight-Ansible-Roles.git
 cd Corelight-Ansible-Roles/
 git checkout 2.0beta
 cd scripts/
 ./initial_ansible_installation_venv.sh
-source /etc/corelight-env/bin/activate
 
-# copy or create variable file
-# copy or create secrets file
-# copy or create inventory
+# copy or create the following files and put them in '/etc/corelight/Corelight-Ansible-Roles/common/'
+# secrets.yml
+# variable files ( .yml)  You can have multiple
+
+# copy or create the following files and put them in '/etc/corelight/Corelight-Ansible-Roles/common/inventory/'
+# inventory files ( .yml)  You can have multiple
+
 # copy or create vault password file
 # edit /etc/ansible/ansible.cfg and uncomment the following line and point to your vault password file.
-#  'vault_password_file = /path/to/vault_password_file'
-#        Note: If you need a password for sudo, add 'ansible_become_pass: '{{centos7_sudo}}' to the inventory file.  In this example,  add a variable called 'centos7_sudo' to your secrets.yml with the real password.  If you need if for localhost, make sure you also add it to the 'default-localhost.yml' inventory file.
+#vault_password_file = /path/to/vault_password_file
 
+# Note: If you need a password for sudo, add 'ansible_become_pass: '{{centos7_sudo}}' to the inventory file.
+# In this example,  add a variable called 'centos7_sudo' to your secrets.yml with the real password.
+# If you need if for localhost, make sure you also add it to the 'default-localhost.yml' inventory file or create an entry for localhost in your inventory file.
 
-
-#  ansible-playbook -i ../common/inventory/ ../install-config-all-main-host.yml  --extra-vars '@../common/secrets.yml'
+# Note: Run the next commands after Ansible has been configured as noted above.
+# source /etc/corelight-env/bin/activate
+# cd /etc/corelight-env/Corelight-Ansible-Roles/scripts/
+# ansible-playbook -i ../common/inventory/ ../install-config-all-main-host.yml  --extra-vars '@../common/secrets.yml'
