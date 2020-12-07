3bfe7c416e15fe0cb02d7b251c511251798c250c
James Lagermann <james.lagermann@corelight.com>
2020-12-07 14:54:58 -0600

    updated initial script
    Signed-off-by: James Lagermann <james.lagermann@corelight.com>

--- scripts/initial_ansible_installation_venv.sh
+++ scripts/initial_ansible_installation_venv.sh
@@ -4,53 +4,53 @@ lowercase(){
     echo "$1" | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/"
 }
 
-OS=`lowercase \`uname\``
-KERNEL=`uname -r`
-MACH=`uname -m`
+OS="$(lowercase "$(uname)")"
+KERNEL="$(uname -r)"
+MACH="$(uname -m)"
 
 if [ "${OS}" == "windowsnt" ]; then
   OS=windows
 elif [ "${OS}" == "darwin" ]; then
   OS=mac
 else
-  OS=`uname`
+  OS=$(uname)
   if [ "${OS}" = "SunOS" ] ; then
     OS=Solaris
-    ARCH=`uname -p`
-    OSSTR="${OS} ${REV}(${ARCH} `uname -v`)"
+    ARCH=$(uname -p)
+    OSSTR="${OS} ${REV}(${ARCH} $(uname -v))"
   elif [ "${OS}" = "AIX" ] ; then
-    OSSTR="${OS} `oslevel` (`oslevel -r`)"
+    OSSTR="${OS} $(oslevel) ($(oslevel -r))"
   elif [ "${OS}" = "Linux" ] ; then
     if [ -f /etc/redhat-release ] ; then
       DistroBasedOn='RedHat'
-      DIST=`cat /etc/redhat-release |sed s/\ release.*//`
-      PSUEDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed s/\)//`
-      REV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
-      MREV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*// | sed s/\\..*//`
+      DIST=$(sed s/\ release.*//  /etc/redhat-release)
+      PSUEDONAME=$(sed s/.*\(// /etc/redhat-release | sed s/\)//)
+      REV=$(sed s/.*release\ // /etc/redhat-release | sed s/\ .*//)
+      MREV=$(sed s/.*release\ // /etc/redhat-release | sed s/\ .*// | sed s/\\..*//)
     elif [ -f /etc/SuSE-release ] ; then
       DistroBasedOn='SuSe'
-      PSUEDONAME=`cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//`
-      REV=`cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ //`
-      MREV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*// | sed s/\\..*//`
+      PSUEDONAME=$(tr "\n" ' ' /etc/SuSE-release | sed s/VERSION.*//)
+      REV=$(tr "\n" ' ' /etc/SuSE-release | sed s/.*=\ //)
+      MREV=$(sed s/.*release\ // /etc/redhat-release | sed s/\ .*// | sed s/\\..*//)
     elif [ -f /etc/mandrake-release ] ; then
       DistroBasedOn='Mandrake'
-      PSUEDONAME=`cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//`
-      REV=`cat /etc/mandrake-release | sed s/.*release\ // | sed s/\ .*//`
-      MREV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*// | sed s/\\..*//`
+      PSUEDONAME=$(sed s/.*\(// /etc/mandrake-release | sed s/\)//)
+      REV=$( sed s/.*release\ // /etc/mandrake-release |sed s/\ .*//)
+      MREV=$(sed s/.*release\ // /etc/redhat-release | sed s/\ .*// | sed s/\\..*//)
     elif [ -f /etc/debian_version ] ; then
       DistroBasedOn='Debian'
       if [ -f /etc/lsb-release ] ; then
-              DIST=`cat /etc/lsb-release | grep '^DISTRIB_ID' | awk -F=  '{ print $2 }'`
-                    PSUEDONAME=`cat /etc/lsb-release | grep '^DISTRIB_CODENAME' | awk -F=  '{ print $2 }'`
-                    REV=`cat /etc/lsb-release | grep '^DISTRIB_RELEASE' | awk -F=  '{ print $2 }'`
-                    MREV=`cat /etc/lsb-release | grep '^DISTRIB_RELEASE' | awk -F. '{ print $1 }' | awk -F= '{ print $2 }'`
+              DIST=$(grep '^DISTRIB_ID' /etc/lsb-release | awk -F=  '{ print $2 }')
+                    PSUEDONAME=$(grep '^DISTRIB_CODENAME' /etc/lsb-release | awk -F=  '{ print $2 }')
+                    REV=$(grep '^DISTRIB_RELEASE' /etc/lsb-release | awk -F=  '{ print $2 }')
+                    MREV=$(grep '^DISTRIB_RELEASE' /etc/lsb-release | awk -F. '{ print $1 }' | awk -F= '{ print $2 }')
                 fi
     fi
     if [ -f /etc/UnitedLinux-release ] ; then
-      DIST="${DIST}[`cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//`]"
+      DIST="${DIST}[$(tr "\n" ' ' /etc/UnitedLinux-release | sed s/VERSION.*//)]"
     fi
-    OS=`lowercase $OS`
-    DistroBasedOn=`lowercase $DistroBasedOn`
+    OS=$(lowercase $OS)
+    DistroBasedOn=$(lowercase $DistroBasedOn)
     readonly OS
     readonly DIST
     readonly DistroBasedOn
@@ -63,10 +63,10 @@ else
 
 fi
 
-echo $DIST $REV
+echo "$DIST" "$REV"
 
-if [ $DistroBasedOn = "redhat" ]; then
-        if [ $MREV = "7" ]; then
+if [ "$DistroBasedOn" = "redhat" ]; then
+        if [ "$MREV" = "7" ]; then
                 echo "Installing Python3-pip and other dependencies"
                 sudo yum install -y epel-release libselinux-python dnf
                 sudo dnf install -y -q python3-pip git
@@ -75,7 +75,7 @@ if [ $DistroBasedOn = "redhat" ]; then
                 sudo yum install -y dnf
                 sudo dnf install -y -q python3-pip git
         fi
-elif [ $DistroBasedOn = "debian" ]; then
+elif [ "$DistroBasedOn" = "debian" ]; then
         sudo apt-get update -y -q
         sudo apt-get install -y -q --install-suggests python3-pip git
         sudo apt-get install -y -q --install-suggests python3-venv
@@ -88,13 +88,13 @@ fi
 if ! [ -d /etc/corelight-env/ ] > /dev/null; then
         echo "Creating /etc/corelight-env directory"
         sudo mkdir /etc/corelight-env
-        sudo chown $USER.$USER /etc/corelight-env
+        sudo chown "$USER"."$USER" /etc/corelight-env
 fi
 
 if ! [ -d /etc/ansible/ ] > /dev/null; then
         echo "Creating /etc/ansible directory"
         sudo mkdir /etc/ansible
-        sudo chown $USER.$USER /etc/ansible
+        sudo chown "$USER"."$USER" /etc/ansible
         sudo chmod 755 /etc/ansible
 fi
 
