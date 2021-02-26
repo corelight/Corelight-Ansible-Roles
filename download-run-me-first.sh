d23d69a0c49a8ac699111e8126e77e536359af39
James Lagermann <james.lagermann@corelight.com>
2021-02-26 13:02:44 -0600

    updated run-me-first script
    Signed-off-by: James Lagermann <james.lagermann@corelight.com>

--- download-run-me-first.sh
+++ download-run-me-first.sh
@@ -2,7 +2,74 @@
 # to download and run this script in one command, execute the following:
 # source <( curl https://raw.githubusercontent.com/corelight/Corelight-Ansible-Roles/2.0beta/download-run-me-first.sh)
 
+export LANG=en_US.UTF-8
+
+lowercase(){
+    echo "$1" | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/"
+}
+
+OS="$(lowercase "$(uname)")"
+KERNEL="$(uname -r)"
+MACH="$(uname -m)"
+
+if [ "${OS}" == "windowsnt" ]; then
+  OS=windows
+elif [ "${OS}" == "darwin" ]; then
+  OS=mac
+else
+  OS=$(uname)
+  if [ "${OS}" = "SunOS" ] ; then
+    OS=Solaris
+    ARCH=$(uname -p)
+    OSSTR="${OS} ${REV}(${ARCH} $(uname -v))"
+  elif [ "${OS}" = "AIX" ] ; then
+    OSSTR="${OS} $(oslevel) ($(oslevel -r))"
+  elif [ "${OS}" = "Linux" ] ; then
+    if [ -f /etc/redhat-release ] ; then
+      DistroBasedOn='RedHat'
+      DIST=$(sed s/\ release.*//  /etc/redhat-release)
+      PSUEDONAME=$(sed s/.*\(// /etc/redhat-release | sed s/\)//)
+      REV=$(sed s/.*release\ // /etc/redhat-release | sed s/\ .*//)
+      MREV=$(sed s/.*release\ // /etc/redhat-release | sed s/\ .*// | sed s/\\..*//)
+    elif [ -f /etc/SuSE-release ] ; then
+      DistroBasedOn='SuSe'
+      PSUEDONAME=$(tr "\n" ' ' /etc/SuSE-release | sed s/VERSION.*//)
+      REV=$(tr "\n" ' ' /etc/SuSE-release | sed s/.*=\ //)
+      MREV=$(sed s/.*release\ // /etc/redhat-release | sed s/\ .*// | sed s/\\..*//)
+    elif [ -f /etc/mandrake-release ] ; then
+      DistroBasedOn='Mandrake'
+      PSUEDONAME=$(sed s/.*\(// /etc/mandrake-release | sed s/\)//)
+      REV=$( sed s/.*release\ // /etc/mandrake-release |sed s/\ .*//)
+      MREV=$(sed s/.*release\ // /etc/redhat-release | sed s/\ .*// | sed s/\\..*//)
+    elif [ -f /etc/debian_version ] ; then
+      DistroBasedOn='Debian'
+      if [ -f /etc/lsb-release ] ; then
+              DIST=$(grep '^DISTRIB_ID' /etc/lsb-release | awk -F=  '{ print $2 }')
+                    PSUEDONAME=$(grep '^DISTRIB_CODENAME' /etc/lsb-release | awk -F=  '{ print $2 }')
+                    REV=$(grep '^DISTRIB_RELEASE' /etc/lsb-release | awk -F=  '{ print $2 }')
+                    MREV=$(grep '^DISTRIB_RELEASE' /etc/lsb-release | awk -F. '{ print $1 }' | awk -F= '{ print $2 }')
+                fi
+    fi
+    if [ -f /etc/UnitedLinux-release ] ; then
+      DIST="${DIST}[$(tr "\n" ' ' /etc/UnitedLinux-release | sed s/VERSION.*//)]"
+    fi
+    OS=$(lowercase $OS)
+    DistroBasedOn=$(lowercase $DistroBasedOn)
+    readonly OS
+    readonly DIST
+    readonly DistroBasedOn
+    readonly PSUEDONAME
+    readonly REV
+    readonly MREV
+    readonly KERNEL
+    readonly MACH
+  fi
+
+fi
+
 echo -e "\033[0;33m";
+echo "$DIST" "$REV"
+echo "  "
 echo "The script you are about to run will do the following:"
 echo ""
 echo "  Install or upgrade the following packages depending on the OS"
@@ -19,15 +86,58 @@ read -p "Press any key to continue or CTRL-C to cancel ..."
 echo -e "\033[0m"
 echo ""
 
-sudo mkdir /etc/corelight-env
-cd /etc/corelight-env
-sudo chown $USER.$USER /etc/corelight-env
+
+if [ "$DistroBasedOn" = "redhat" ]; then
+        if [ "$MREV" = "7" ]; then
+                echo "Installing Python3-pip and other dependencies"
+                sudo yum install -y epel-release libselinux-python dnf
+                sudo dnf install -y -q python3-pip git
+                sudo dnf install -y -q libselinux-python3
+        else
+                echo "Installing Python3-pip and other dependencies"
+                sudo yum install -y dnf
+                sudo dnf install -y -q python3-pip git
+        fi
+elif [ "$DistroBasedOn" = "debian" ]; then
+        sudo apt-get update -y -q
+        sudo apt-get install -y -q --install-suggests python3-pip git
+        sudo apt-get install -y -q --install-suggests python3-venv
+else
+        echo "Not RedHat or Debian based"
+        exit 1
+fi
+
+if ! [ -d /etc/corelight-env/ ] > /dev/null; then
+        echo "Creating /etc/corelight-env directory"
+        sudo mkdir /etc/corelight-env
+        sudo chown "$USER"."$USER" /etc/corelight-env
+fi
+
+if ! [ -d /etc/ansible/ ] > /dev/null; then
+        echo "Creating /etc/ansible directory"
+        sudo mkdir /etc/ansible
+        sudo chown "$USER"."$USER" /etc/ansible
+        sudo chmod 755 /etc/ansible
+fi
+
+
+echo "Creating python3 virtual environment"
+python3 -m venv /etc/corelight-env
+source /etc/corelight-env/bin/activate
+python3 -m pip install --upgrade pip wheel setuptools
+
+echo "Installing Ansible"
+python3 -m pip install --upgrade --upgrade-strategy eager ansible
+ansible-galaxy collection install community.general -c
+
+echo "Cloning Corelight-Ansible-Roles 2.0beta Branch"
 cd /etc/corelight-env
 git clone https://github.com/corelight/Corelight-Ansible-Roles.git
 cd Corelight-Ansible-Roles/
 git checkout 2.0beta
-cd scripts-initial-setup/
-./initial_ansible_installation_venv.sh
+
+echo "Coping Ansible default config to /etc/ansible/ansible/cfg"
+cp ../roles/ansible_install/files/default-ansible.cfg /etc/ansible/ansible.cfg
 
 
 echo -e "\033[1;32m";
@@ -79,7 +189,14 @@ echo -e "\033[1;32m     cd /etc/corelight-env/Corelight-Ansible-Roles/scripts-in
 echo -e "\033[1;32m     ansible-playbook -i ../common/inventory/ ./ssh-copy-id-to-all.yml  --extra-vars '@../common/secrets.yml'";
 echo -e "\033[0;33m";
 read -p "Press any key for more options ..."
-clear
+echo "  "
+echo "  "
+echo "  "
+echo "  "
+echo "  "
+echo "  "
+echo "  "
+echo "  "
 echo "                               ";
 echo "    ((((                       ";
 echo "  ((     _                     ";
